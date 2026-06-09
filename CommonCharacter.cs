149007c3a21789a6e169a8790d48fe4c73b41332
meideka20 <70358214+meideka20@users.noreply.github.com>
2026-06-09 14:33:44 -0400

    // Code added

---
+++ CommonCharacter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterBattle
+{
+    class CommonCharacter
+    {
+        protected int strength;
+        protected int health;
+        protected string name;
+        protected Random random;
+
+        public int Strength
+        {
+            get { return strength; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                health = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public CommonCharacter(int strength, int health, string name, Random random)
+        {
+            this.random = random;
+            if (strength < 0)
+            {
+                this.strength = 10;
+            }
+            else
+            {
+                this.strength = strength;
+            }
+            if (health < 0)
+            { 
+                this.health = 100;
+            }
+            else
+            {
+                this.health = health;
+            }
+            if (name == "")
+            {
+                this.name = "Billy";
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public virtual int Attack()
+        {
+            return random.Next(0, strength);
+        }
+
+        public virtual void TakeDamage(int amount)
+        {
+            health -= amount;
+            if(health < 0)
+            {
+                health = 0;
+            }
+        }
+
+        public virtual bool ReadyToFlee()
+        {
+            if(health >=50)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsDead()
+        {
+            if(health <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return ("Character stats: " + name + " has " + strength + " strength and has " + health + " health left.");
+        }
+    }
+}
---
+++ Warrior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterBattle
+{
+    class Warrior : CommonCharacter
+    {
+        private int bonusDamage;
+        private int armor;
+
+        public int BonusDamage
+        {
+            get { return bonusDamage; }
+            set { bonusDamage = value; }
+        }
+
+        public int Armor
+        {
+            get { return armor; }
+            set { armor = value; }
+        }
+
+        public Warrior(int strength, int health, string name, Random random, int bonusDamage) : base(strength, health, name, random)
+        {
+            this.bonusDamage = bonusDamage;
+            this.armor = 3;
+        }
+
+        public override bool ReadyToFlee()
+        {
+            return false;
+        }
+
+        public override int Attack()
+        {
+            return (random.Next(0, strength+1) + bonusDamage);
+        }
+
+        // will always take damage minus the armor rating
+        public override void TakeDamage(int amount)
+        {
+            if (amount - armor > 0)
+            {
+                health -= amount - armor;
+                if (health < 0)
+                {
+                    health = 0;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" + (" They are a Warrior that gets " + bonusDamage + " bonus damage when attacking." + "\n" + " Their armor reduces incoming damage by " + armor + ".");
+        }
+    }
+}
